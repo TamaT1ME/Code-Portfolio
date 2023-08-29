41fc73ef9be9e571c3de60668bca1d1a408fef22
MJ <111803884+TamaT1ME@users.noreply.github.com>
2023-08-29 12:59:46 -0700

    Delete HTML-CSS-PA14.js

--- HTML-CSS-PA14.js
+++
@@ -1,8 +0,0 @@
-
-function counter() {
-    console.log('Clicked');
-    let count = document.getElementById('counter');
-    let number = Number(count.innerHTML);
-    number += 1;
-    count.innerHTML = String(number)
-}
